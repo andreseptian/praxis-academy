3e620b2cc543947ba9d41eacf893e22f8115aca5
andreseptian <andre.septian2016@student.uny.ac.id>
2020-08-20 16:50:20 +0700

    Hari Kedua

---
+++ novice/01-02/kasus/kasus-1/Create.java
@@ -0,0 +1,71 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class Create {
+
+    public static void main(String argv[]) {
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+
+            // root element
+            Element rootElement = doc.createElement("cars");
+            doc.appendChild(rootElement);
+
+            // supercars element
+            Element supercar = doc.createElement("supercars");
+            rootElement.appendChild(supercar);
+
+            // setting attribute to element
+            Attr attr = doc.createAttribute("company");
+            attr.setValue("Ferrari");
+            supercar.setAttributeNode(attr);
+
+            // carname element
+            Element carname = doc.createElement("carname");
+            Attr attrType = doc.createAttribute("type");
+            attrType.setValue("formula one");
+            carname.setAttributeNode(attrType);
+            carname.appendChild(doc.createTextNode("Ferrari 101"));
+            supercar.appendChild(carname);
+
+            Element carname1 = doc.createElement("carname");
+            Attr attrType1 = doc.createAttribute("type");
+            attrType1.setValue("sports");
+            carname1.setAttributeNode(attrType1);
+            carname1.appendChild(doc.createTextNode("Ferrari 202"));
+            supercar.appendChild(carname1);
+
+            Element carname2 = doc.createElement("carname");
+            Attr attrType2 = doc.createAttribute("type");
+            attrType2.setValue("sports");
+            carname2.setAttributeNode(attrType2);
+            carname2.appendChild(doc.createTextNode("Ferrari 203"));
+            supercar.appendChild(carname2);
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("cars.xml"));
+            StreamResult result1 = new StreamResult(new File("cars.json"));
+            transformer.transform(source, result);
+            transformer.transform(source, result1);
+            // Output to console for testing
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
