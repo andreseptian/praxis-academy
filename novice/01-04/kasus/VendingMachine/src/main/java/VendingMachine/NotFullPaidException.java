729a363b930df8b5280b4489f8dbe1982ee1d535
andreseptian <andre.septian2016@student.uny.ac.id>
2020-08-25 10:30:54 +0700

    hari keempat

---
+++ novice/01-04/kasus/VendingMachine/src/main/java/VendingMachine/NotFullPaidException.java
@@ -0,0 +1,20 @@
+package VendingMachine;
+
+public class NotFullPaidException extends RuntimeException {
+    private final String message;
+    private final long remaining;
+
+    public NotFullPaidException(String message, long remaining) {
+        this.message = message;
+        this.remaining = remaining;
+    }
+
+    public long getRemaining() {
+        return remaining;
+    }
+
+    @Override
+    public String getMessage() {
+        return message + remaining;
+    }
+}
---
+++ novice/01-04/kasus/VendingMachine/src/main/java/VendingMachine/NotSufficientChangeException.java
@@ -0,0 +1,14 @@
+package VendingMachine;
+
+public class NotSufficientChangeException extends RuntimeException {
+    private final String message;
+
+    public NotSufficientChangeException(String string) {
+        this.message = string;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
